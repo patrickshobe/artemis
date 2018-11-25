6dc335182bd44e663cb61504026bdda94de7be79
Patrick Shobe <patrick@patrickshobe.com>
2018-11-25 15:16:38 -0700

    Fixes tests

--- spec/services/episode_builder_spec.rb
+++ spec/services/episode_builder_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 describe 'Epside Builder Service' do
   it 'should be able to build episodes' do
     VCR.use_cassette('episde_builder_build') do
-      sb = SeriesBuilder.new
-      sb.build_all_series
+      SeriesBuilder.build_all
     end
 
     VCR.use_cassette('episodes') do
-      eb = EpisodeBuilder.new
-      episodes = eb.build_all_episodes
+      episodes = EpisodeBuilder.build_all
 
       expect(Episode.count).to be > 40
     end
--- spec/services/sonarr_interface_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-describe 'Sonarr Interface Service' do
-end
